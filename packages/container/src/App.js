ac6cbe535c8c289ab645d6e5f53608f21e4dae8f
Arunv8001 <a55001n@gmail.com>
2024-02-15 07:52:22 +0100

    inital commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarketingComponent from './components/MarketingComponent';
+
+export default () => {
+    return (<>
+    <h1>Hi There!</h1>
+    <hr />
+    <MarketingComponent />
+    </>)
+}
